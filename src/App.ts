8a74544d4f020d9666a9220050e6d89bc5da22d0
Brennon J <juniobrennon@gmail.com>
2023-08-20 16:30:35 -0400

    usando morgan para logs das apis

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,7 @@
 import express, { Request, NextFunction, Response } from "express";
 import { router as routes } from "../src/routes/routes";
+const morgan = require("morgan");
 require("dotenv").config();
-
-
 
 const app = express();
 const PORT = process.env.PORT_SERVER || 4000;
@@ -10,20 +9,13 @@ const PORT = process.env.PORT_SERVER || 4000;
 // Middleware para fazer o parsing do corpo da requisição como JSON
 app.use(express.json());
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.on("finish", () => {
-    const status = res.statusCode;
-    const message = res.statusMessage;
-
-  });
-
-  next();
-});
+// Configurar o morgan para registrar solicitações HTTP
+app.use(morgan("dev"));
 
 // Definir as rotas
 app.use(routes);
- 
+
 // Iniciar o servidor
-app.listen( PORT, () => {
-  console.log(`Serviço rodando na porta: ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Rodando na porta: ${PORT}`);
 });
