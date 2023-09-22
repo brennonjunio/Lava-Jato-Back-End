6ece5878cc335c586fc6ee3228a32e84668543cc
Brennon Junio <juniobrennon@gmail.com>
2023-09-22 16:35:40 -0400

    [usuarios]passando parametros/useCase Crypt

--- src/modules/usuarios/repository/useCase.ts
+++ src/modules/usuarios/repository/useCase.ts
@@ -1,9 +1,16 @@
 import db from "../../../database/database";
-export class UseCaseUsuarios{
-async usuarioJaCriado(param:string){
-  const  result = await db.usuarios.findFirst({
-        where:{email:param}
-    })
-        return result;
-}
+const bcrypt = require("bcrypt");
+
+export class UseCaseUsuarios {
+  async usuarioJaCriado(param: string) {
+    const result = await db.usuarios.findFirst({
+      where: { email: param },
+    });
+    return result;
+  }
+  async criptSenha(param: string) {
+    const salt = await bcrypt.genSalt(12);
+    const senha = await bcrypt.hash(param, salt);
+    return senha;
+  }
 }
--- src/modules/usuarios/repository/usuariosRepository.ts
+++ src/modules/usuarios/repository/usuariosRepository.ts
@@ -1,12 +1,14 @@
 import db from "../../../database/database";
 import { CriarUsuarioDTO } from "../dto/usuariosDTO";
-const bcrypt = require("bcrypt");
+import { UseCaseUsuarios } from "./useCase";
 
 export class UsuariosRepository {
+  private case: UseCaseUsuarios = new UseCaseUsuarios();
+
   async criarUsuarios(params: CriarUsuarioDTO) {
-    const salt = await bcrypt.genSalt(12);
-    const senha = await bcrypt.hash(params.senha, salt);
+    const senha = await this.case.criptSenha(params.senha);
     const result = await db.usuarios.create({
+      
       data: { nm_usuario: params.nm_usuario, senha: senha },
     });
     return result;
