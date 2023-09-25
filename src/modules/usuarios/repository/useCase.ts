1f992e142946d4dcfe089556f0f5a8253b5faafe
Brennon Junio <juniobrennon@gmail.com>
2023-09-25 16:39:20 -0400

    [usuario]-criando valdação para nm_usuario unique

--- src/modules/usuarios/repository/useCase.ts
+++ src/modules/usuarios/repository/useCase.ts
@@ -2,9 +2,15 @@ import db from "../../../database/database";
 const bcrypt = require("bcrypt");
 
 export class UseCaseUsuarios {
-  async usuarioJaCriado(param: string) {
+  async emailEmUso(param: string) {
     const result = await db.usuarios.findFirst({
       where: { email: param },
+    });
+    return result;
+  }
+  async userEmUso(param: string) {
+    const result = await db.usuarios.findFirst({
+      where: { nm_usuario: param },
     });
     return result;
   }
