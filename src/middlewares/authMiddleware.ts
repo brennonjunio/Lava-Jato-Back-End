2635420b307d5e7da74a9d1e28e0576efb1b0b19
Brennon Junio <juniobrennon@gmail.com>
2023-09-25 15:20:52 -0400

    [auth]- retirando const não usada

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -4,17 +4,12 @@ import { NextFunction, Request, Response } from 'express';
 
 export class AuthMiddleware {
     static varify(req: Request, res: Response, next: NextFunction) {
-        const excludedRoutes = ['/usuario']; 
-
-        if (excludedRoutes.includes(req.path)) {
-          return next();
-        }
         const result = new AuthService().validaToken(
             req.headers['authorization'],
         );
         console.error(result.err);
         if (!result.isValidToken) {
-            return res.status(401).json({ status: false, message: 'Token Invalido Ou não encontrado'});
+            return res.status(401).json({ status: false, message: 'Token não foi encontrado ou é invalido'});
         }
 
         next();
