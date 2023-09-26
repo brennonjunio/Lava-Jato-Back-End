5284ea021c7af57fdc31996a371d076d8fb8a174
Brennon Junio <juniobrennon@gmail.com>
2023-09-26 00:21:52 -0400

    [auth]-token

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -5,8 +5,9 @@ import { NextFunction, Request, Response } from 'express';
 export class AuthMiddleware {
     static varify(req: Request, res: Response, next: NextFunction) {
         const result = new AuthService().validaToken(
-            req.headers['authorization'],
+            req.headers.authorization,
         );
+
         console.error(result.err);
         if (!result.isValidToken) {
             return res.status(401).json({ status: false, message: 'Token não foi encontrado ou é invalido'});
