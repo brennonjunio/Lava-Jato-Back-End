792494d433003121df4c23a13c7e3637cc0bfa4c
Brennon Junio <juniobrennon@gmail.com>
2023-09-27 19:15:44 -0400

    [AGENDAR-SERVICOS]- nova forma de agendar

--- src/modules/servicos/repository/agendamentoServicosRepository.ts
+++ src/modules/servicos/repository/agendamentoServicosRepository.ts
@@ -7,25 +7,33 @@ export class agendamentoServicosRepository {
   private useCase: useCaseAgendamento = new useCaseAgendamento();
 
   async agendarServico(p: criarAgendamentoServicoDTO) {
-    const result = await db.$queryRawUnsafe(
-      "select agendar_servico(:cd_servico_p, :cd_agenda_p, :cd_cliente_p, :cd_veiculo_p, :placa_p, :cd_usuario_p)",
-      p.cd_servico_p,
+    const result = (await db.$queryRawUnsafe(
+      "select agendar_servico(:cd_agenda_p, :cd_cliente_p, :cd_usuario_p) as sequencia",
       p.cd_agenda_p,
       p.cd_cliente_p,
-      p.cd_servico_p,
-      p.placa_p,
       p.cd_usuario_p
-    );
-    return result;
+    )) as { sequencia: number }[];
+
+    for (const cd_servico of p.cd_servico_p) {
+      await db.$queryRawUnsafe(
+        "insert into servicos_atendimento (nr_seq_atendimento,cd_servico,cd_veiculo,placa,cd_usuario) values (?,?,?,?,?)",
+        result[0].sequencia,
+        cd_servico,
+        p.cd_veiculo_p,
+        p.placa_p,
+        p.cd_usuario_p
+      );
+    }
+
+    return true;
   }
   async listarServicosAgendados() {
     const result = await db.atendimentos.findMany({
       include: {
         clientes: { select: { nm_cliente: true } },
-        
       },
       where: {
-        status_servico: "A"
+        status_servico: "A",
       },
     });
 
@@ -46,7 +54,7 @@ export class agendamentoServicosRepository {
 
   async listarServicosFinalizados() {
     const result = await db.atendimentos.findMany({
-      where:{status_servico:"F"},
+      where: { status_servico: "F" },
       include: { clientes: { select: { nm_cliente: true } } },
     });
     return result;
