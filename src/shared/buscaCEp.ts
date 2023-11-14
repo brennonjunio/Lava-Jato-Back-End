dd667758f8c0af107a0c9b5280bef4aa2aef6b42
Brennon Junio <juniobrennon@gmail.com>
2023-11-14 01:05:03 -0400

    validando tamanho do campo na utils Buscar Cep

--- src/shared/buscaCEp.ts
+++ src/shared/buscaCEp.ts
@@ -2,13 +2,16 @@ import { Request, Response } from "express";
 import axios from "axios";
 export const buscarCep = async (req: Request, res: Response) => {
   const { cep } = req.params;
+  if(cep.length > 8 || cep.length <  8){
+    return res.status(400).json({message:"Cep deve conter 8 digitos"})
+  }
   const result = await axios({
     method: "get",
     url: `https://viacep.com.br/ws/${cep}/json/`,
   });
 
 if(result.data.erro == 'true'){
-    return res.status(400).json({error: "Cep Inexistente ou invalido"});   
+    return res.status(400).json({message: "Cep Inexistente ou invalido"});   
 }
   return res.status(200).json(result.data);
 };
